60251c887de7d41ac9a4ca7a0fc35d2c28e6300d
FrankReiser <frank@thereisers.com>
2021-03-05 18:51:35 -0500

    Oversight Rectification
    I must have failed to do a clean build after moving all of my include files back into the source directory.

--- tests/testSemaphore.cpp
+++ tests/testSemaphore.cpp
@@ -3,7 +3,7 @@
 //
 
 // What we are testing
-#include "ReiserRT_Core/Semaphore.hpp"
+#include "Semaphore.hpp"
 
 // Test task class specifications for give and taking the semaphore.
 #include "SemTestTasks.h"
