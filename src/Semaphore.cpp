bd6c425d9ad5341d6d35b0a64306c2596c14b6c0
FrankReiser <frank@thereisers.com>
2021-06-11 21:22:46 -0400

    Reorganize Semaphore Implementation
    Reorganize the Semaphore implementation to avoid CTidy complaints plus some attribute renaming for the next possible attempt to make the Semaphore bi-polar (block on notify limit). This was attempted before and abandoned. I am going to attempt to resurrect it but may not be successful. I want to at least capture these changes.

--- src/Semaphore.cpp
+++ src/Semaphore.cpp
@@ -40,7 +40,26 @@ public:
     * @param theInitialCount The initial Semaphore count, defaults to zero and is clamped to
     * std::numeric_limits< AvailableCountType>::max() or slightly more than 4 billion.
     */
-    explicit Imple( size_t theInitialCount = 0 );
+    explicit Imple( size_t theInitialCount = 0 )
+        : waitConditionVar{}
+        , mutex{}
+        , availableCount{ theInitialCount > std::numeric_limits< AvailableCountType >::max() ?
+                          std::numeric_limits< AvailableCountType >::max() : AvailableCountType( theInitialCount ) }
+        , waitPendingCount{0 }
+        , abortFlag{ false }
+    {
+#ifdef REISER_RT_HAS_PTHREADS
+        // Initialize a condition variable attribute
+        pthread_condattr_t attr;
+        pthread_condattr_init( &attr );
+
+        // Initialize condition variable
+        pthread_cond_init(&waitConditionVar, &attr );
+
+        // Destroy attribute, we are done with it.
+        pthread_condattr_destroy( &attr );
+#endif
+    }
 
     /**
     * @brief Destructor for the Implementation
@@ -49,7 +68,18 @@ public:
     * used by any thread when this destructor is invoked, those threads could experience
     * ReiserRT::Core::SemaphoreAborted being thrown.
     */
-    ~Imple();
+    ~Imple()
+    {
+        abort();
+
+        // Sleep a small amount to allow waiters to get out of the way.
+        std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
+
+#ifdef REISER_RT_HAS_PTHREADS
+        // Destroy the condition variable.
+        pthread_cond_destroy( &waitConditionVar );
+#endif
+    }
 
     /**
     * @brief Copy Constructor for Implementation
@@ -141,10 +171,10 @@ public:
     inline void wait( FunctionType & operation ) {
         std::unique_lock< Mutex > lock{ mutex };
         _wait( lock );
-#if 1
+
         struct AvailableCountManager {
-            AvailableCountManager( AvailableCountType & theAC ) : rAC(theAC) {}
-            ~AvailableCountManager() { if ( !released ) ++rAC; }
+            explicit AvailableCountManager( AvailableCountType & theAC ) noexcept : rAC(theAC)  {}
+            ~AvailableCountManager() noexcept { if ( !released ) ++rAC; }
 
             void release() { released = true; }
 
@@ -156,16 +186,6 @@ public:
         AvailableCountManager availableCountManager{ availableCount };
         operation();
         availableCountManager.release();
-#else
-        try {
-            operation();
-        }
-        catch ( std::exception & )
-        {
-            ++availableCount;
-            throw;
-        }
-#endif
     }
 
     /**
@@ -197,7 +217,21 @@ public:
     * Otherwise, sets the abortFlag and invokes the conditionVal, notify_all operation to wake all waiting threads.
     * The mutex is released upon return.
     */
-    void abort();
+    void abort()
+    {
+        std::lock_guard< Mutex > lock( mutex );
+
+        // If the abort flag is set, quietly get out of the way.
+        if ( abortFlag ) return;
+
+        abortFlag = true;
+        if (waitPendingCount != 0 )
+#ifdef REISER_RT_HAS_PTHREADS
+            pthread_cond_broadcast( &waitConditionVar );
+#else
+        waitConditionVar.notify_all();
+#endif
+    }
 
     /**
     * @brief Get Available Count
@@ -208,50 +242,97 @@ public:
     *
     * @return Returns a snapshot of the availableCount at time of invocation.
     */
-    size_t getAvailableCount();
+    size_t getAvailableCount()
+    {
+        std::lock_guard< Mutex > lock( mutex );
+
+        return availableCount;
+    }
 
 private:
     /**
     * @brief The Wait Operation Internals
     *
-    * This operation is for internal use. It expects that our mutex has been acquired prior to invocation.
+    * This operation is for internal use by the outer "wait" operations.
+    * It expects that our mutex has been acquired prior to invocation.
     * The operation attempts to decrement the availableCount towards zero and then returns.
-    * If the availableCount is zero, the pendingCount is incremented and then the call blocks on our conditionVar simultaneously
+    * If the availableCount is zero, the waitPendingCount is incremented and then the call blocks on our waitConditionVar simultaneously
     * unlocking the mutex until notified to unblock.
-    * Once notified, the mutex is re-taken, the pendingCount is decremented and we re-loop attempting to
+    * Once notified, the mutex is re-taken, the waitPendingCount is decremented and we re-loop attempting to
     * decrement the availableCount towards zero once more.
     *
     * @throw Throws ReiserRT::Core::SemaphoreAborted if the abortFlag has been set via the abort operation.
     */
-    void _wait( std::unique_lock< Mutex > & lock );
+    void _wait( std::unique_lock< Mutex > & lock )
+    {
+#ifdef REISER_RT_HAS_PTHREADS
+        // The code involved with obtaining this native handle is largely or completely inlined
+        // and then significantly optimized away.
+        auto mutexNativeHandle = lock.mutex()->native_handle();
+#endif
+        for (;;)
+        {
+            // If the abort flag is set, throw a SemaphoreAbortedException.
+            if ( abortFlag ) throw SemaphoreAborted{ "Semaphore::Imple::_wait: Semaphore Aborted!" };
+
+            // If the available count is greater than zero, decrement it and and escape out.
+            // We have "taken" the semaphore.  Waiting is not necessary.
+            if ( availableCount > 0 )
+            {
+                --availableCount;
+                return;
+            }
+
+            // Else we must wait for a notification.
+            ++waitPendingCount;
+#ifdef REISER_RT_HAS_PTHREADS
+            pthread_cond_wait(&waitConditionVar, mutexNativeHandle );
+#else
+            waitConditionVar.wait( lock, [ this ]{ return abortFlag || availableCount > 0; } );
+#endif
+            // Awakened with test returning true.
+            --waitPendingCount;
+        }
+    }
 
     /**
     * @brief The Notify Operation Internals
     *
-    * This operation is for internal use. It expects that our mutex has been acquired prior to invocation.
-    * The operation increments the availableCount and if pendingCount is
-    * greater than zero, invokes the conditionVar, notify_one operation to wake one waiting thread.
+    * This operation is for internal use by the outer "notify" operations.
+    * It expects that our mutex has been acquired prior to invocation.
+    * The operation increments the availableCount and if waitPendingCount is
+    * greater than zero, invokes the waitConditionVar, notify_one operation to wake one waiting thread.
     *
     * @throw Throws ReiserRT::Core::SemaphoreAborted if the abortFlag has been set via the abort operation.
     */
-    void _notify();
+    void _notify()
+    {
+        // If the abort flag is set OR we have been "over notified", throw a runtime error.
+        if ( abortFlag || availableCount == std::numeric_limits< AvailableCountType >::max() ) {
+            throw SemaphoreAborted{ abortFlag ? "Semaphore::Imple::_notify: Semaphore Aborted!" :
+                "Semaphore::Imple::_notify: Notification Limit Hit!" };
+        }
+
+        ++availableCount;
+
+#ifdef REISER_RT_HAS_PTHREADS
+        pthread_cond_signal( &waitConditionVar );
+#else
+        waitConditionVar.notify_one();
+#endif
+    }
 
     /**
-    * @brief The Condition Variable
+    * @brief The Wait Condition Variable
     *
-    * This is an instance of a C++11 std::condition_variable. On Linux systems, it is a light weight
-    * wrapper around a POSIX condition variable and functions as one would expect. Condition variables
-    * are design to allow threads of execution to efficiently wait (block) until a "condition" becomes true.
-    * The operate in conjunction with a mutex.
+    * This is our wait condition variable that we can block on should the available count be zero.
     */
-    ConditionVarType conditionVar;
+    ConditionVarType waitConditionVar;
 
     /**
     * @brief The Mutex
     *
-    * This contains an instance of a POSIX mutex on Linux systems.
-    * Mutexes are designed to provide mutual exclusion around sections of code to ensure coherency
-    * of data accessed in multi-threaded environments.
+    * This is our mutual exclusion object used to synchronize access to our state attributes.
     */
     Mutex mutex;
 
@@ -265,11 +346,11 @@ private:
     AvailableCountType availableCount;
 
     /**
-    * @brief The PendingCount
+    * @brief The Pending Wait Count
     *
-    * This attribute indicates how many threads are pending on our condition variable conditionVar
+    * This attribute indicates how many threads are pending on our condition variable waitConditionVar
     */
-    PendingCountType pendingCount;
+    PendingCountType waitPendingCount;
 
     /**
     * @brief The Abort Flag
@@ -278,113 +359,6 @@ private:
     */
     bool abortFlag;
 };
-
-
-Semaphore::Imple::Imple( size_t theInitialCount )
-    : conditionVar{}
-    , mutex{}
-    , availableCount{ theInitialCount > std::numeric_limits< AvailableCountType >::max() ?
-                    std::numeric_limits< AvailableCountType >::max() : AvailableCountType( theInitialCount ) }
-    , pendingCount{ 0 }
-    , abortFlag{ false }
-{
-#ifdef REISER_RT_HAS_PTHREADS
-    // Initialize a condition variable attribute
-    pthread_condattr_t attr;
-    pthread_condattr_init( &attr );
-
-    // Initialize condition variable
-    pthread_cond_init( &conditionVar, &attr );
-
-    // Destroy attribute, we are done with it.
-    pthread_condattr_destroy( &attr );
-#endif
-}
-
-Semaphore::Imple::~Imple()
-{
-    abort();
-
-    // Sleep a small amount to allow waiters to get out of the way.
-    std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
-
-#ifdef REISER_RT_HAS_PTHREADS
-    // Destroy the condition variable.
-    pthread_cond_destroy( &conditionVar );
-#endif
-}
-
-void Semaphore::Imple::_wait( std::unique_lock< Mutex > & lock )
-{
-#ifdef REISER_RT_HAS_PTHREADS
-    // The code involved with obtaining this native handle is largely or completely inlined
-    // and then significantly optimized away.
-    auto mutexNativeHandle = lock.mutex()->native_handle();
-#endif
-    for (;;)
-    {
-        // If the abort flag is set, throw a SemaphoreAbortedException.
-        if ( abortFlag ) throw SemaphoreAborted{ "Semaphore::Imple::_wait: Semaphore Aborted!" };
-
-        // If the available count is greater than zero, decrement it and and escape out.
-        // We have "taken" the semaphore.  Waiting is not necessary.
-        if ( availableCount > 0 )
-        {
-            --availableCount;
-            return;
-        }
-
-        // Else we must wait for a notification.
-        ++pendingCount;
-#ifdef REISER_RT_HAS_PTHREADS
-        pthread_cond_wait( &conditionVar, mutexNativeHandle );
-#else
-        conditionVar.wait( lock, [ this ]{ return abortFlag || availableCount > 0; } );
-#endif
-        // Awakened with test returning true.
-        --pendingCount;
-    }
-}
-
-void Semaphore::Imple::_notify()
-{
-    // If the abort flag is set OR we have been "over notified", throw a runtime error.
-    if ( abortFlag || availableCount == std::numeric_limits< AvailableCountType >::max() ) {
-        throw SemaphoreAborted{ abortFlag ?
-            "Semaphore::Imple::_notify: Semaphore Aborted!" : "Semaphore::Imple::_notify: Notification Limit Hit!" };
-    }
-
-    ++availableCount;
-
-#ifdef REISER_RT_HAS_PTHREADS
-    pthread_cond_signal( &conditionVar );
-#else
-    conditionVar.notify_one();
-#endif
-}
-
-void Semaphore::Imple::abort()
-{
-    std::lock_guard< Mutex > lock( mutex );
-
-    // If the abort flag is set, quietly get out of the way.
-    if ( abortFlag ) return;
-
-    abortFlag = true;
-    if ( pendingCount != 0 )
-#ifdef REISER_RT_HAS_PTHREADS
-        pthread_cond_broadcast( &conditionVar );
-#else
-        conditionVar.notify_all();
-#endif
-}
-
-size_t Semaphore::Imple::getAvailableCount()
-{
-    std::lock_guard< Mutex > lock( mutex );
-
-    return availableCount;
-}
 
 Semaphore::Semaphore( size_t theInitialCount )
     : pImple{ new Semaphore::Imple{ theInitialCount } }
